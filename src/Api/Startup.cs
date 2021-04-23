322e426da7e12f8c53687ec3f1ce36e565048cb4
Ahmad Ilman Fadilah <ahmadilmanfadilah@outlook.com>
2021-04-23 09:51:20 +0700

    add cors

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -66,11 +66,19 @@ namespace todo_api.Api
 
                 configure.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
             });
+
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("MyPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
